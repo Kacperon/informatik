d58e2bfd1d79678fad0320dcfc4e3f1f9ce7456f
Kacperon <kacper.feliks.455@gmail.com>
2023-11-14 12:01:22 +0100

    kolos

---
+++ WDI/kolosy/zadb1.py
@@ -0,0 +1,23 @@
+T1=[7,13,17,19,23,2,6,4,8,3,5,7,1,3,2] 
+T2=[]
+def zad(t):
+    n=len(t)
+    i=0
+    tmaxmax,tminmin=10**10,0
+    while i<n-1:
+        flag=False
+        tmin=t[i]
+        while t[i]<t[i+1]:
+            tmax=t[i+1]
+            i+=1
+            flag=True
+        if flag:
+            if tmin>tminmin:
+                tminmin=tmin
+            if tmax<tmaxmax:
+                tmaxmax=tmax
+        i+=1
+    if tminmin>tmaxmax:
+        return True
+    return False
+print(zad(T2))
--- WDI/test/test.py
+++ WDI/test/test.py
@@ -1,1 +1,5 @@
-print("hello")
+def ejest(x):
+
+    e=(1+1/x)**x
+    return e
+print(ejest(1000000000000000))
